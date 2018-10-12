5b2ad9c78039a871d30812df605c820f5363ebf8
david <david@LAPTOP-GDTN1M92>
2018-10-12 12:11:00 +0200

    hello world

--- src/startGitHub/HelloWorld.java
+++ src/startGitHub/HelloWorld.java
@@ -3,5 +3,8 @@ package startGitHub;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
